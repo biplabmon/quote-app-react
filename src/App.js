27d363b4179f8693ba97c85477a9576d2d4168a3
Biplab <biplab8629@gmail.com>
2023-01-29 20:53:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './Card/Card';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Card />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Card/Card.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import './Card.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+
+const Card = () => {
+
+    const [newQuot, setNewQuot] = useState({
+        quote: '',
+        author: ''
+    });
+
+    useEffect(() =>{
+        fetchQuote();
+    },[]);
+
+    const fetchQuote = () => {
+        axios.get('https://api.quotable.io/random')
+        .then(response =>{
+            const { content } = response.data;
+            const { author } = response.data;
+            setNewQuot({quote: content, author: author})
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    };
+
+    const newQuoteHandaler = () => {
+        fetchQuote();
+    };
+
+    const twitterHandaler = () => {
+        const twitterUrl = `https://twitter.com/intent/tweet?text=${newQuot.quote}  ${'~' + newQuot.author}`;
+        window.open(twitterUrl, '_blank');
+    };
+
+    const whatsappHandaler = () => {
+        const whatsappUrl = `whatsapp://send?text=${newQuot.quote}  ${'~' + '*'+newQuot.author+'*'}`;
+        window.open(whatsappUrl, '_blank');
+    }
+
+
+    return (
+        <>
+            <div className="card_main">
+                <div className="quote_text">
+                    <p> {newQuot.quote} </p>
+                </div>
+                <div className="quote_author">
+                    <p>~ {newQuot.author} </p>
+                </div>
+                <div className="new_quote">
+                    <div className="socisl_button">
+                        <button onClick={twitterHandaler}> <FontAwesomeIcon icon={faTwitter} /> </button>
+                        <button onClick={whatsappHandaler}> <FontAwesomeIcon icon={faWhatsapp} /> </button>
+                    </div>
+                    <div className="quote_button">
+                    <button onClick={newQuoteHandaler}>New Quote!</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default Card;
